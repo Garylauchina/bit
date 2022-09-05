682eacf7451e6c8423084aa613c3bb841561e93f
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-05 16:03:11 +0800

    robot 1.1

--- robot.py
+++ robot.py
@@ -3,23 +3,19 @@ import werobot
 robot = werobot.WeRoBot(token='Dcbpes2098')
 
 
-@robot.handler(f=any())
-def repeat(f):
-    if f == '1':
+@robot.handler
+def echo(msg):
+    if msg.content == '1':
         return '正在查询，请稍后。。。'
     else:
         return '1---广西电信招标'
 
 
-
-
-
 @robot.subscribe
 def options(msg):
-        return '1---广西电信招标'
+    return '1---广西电信招标'
 
 
 robot.config['HOST'] = '0.0.0.0'
 robot.config['PORT'] = 80
 robot.run()
-
