d86c53aa022443a00234ef3559c02d17bba6165a
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-08 23:20:43 +0800

    robot 1.1 每三十分钟更新一次招标信息

--- robot.py
+++ robot.py
@@ -13,13 +13,19 @@ user_list = all_user(access_token)
 user_label = {}
 user_store = []
 today_list = sunshine_list()  # 这是一个列表，里面的元素是字典,存储了当天的招标信息，需定时更新
+today_list_time = time.time()  #上次招标信息更新的时间
 print('共搜索到%s条招标信息' % len(today_list))
 for i in user_list:
     user_label['openid'] = i
     user_label['last_send'] = 0
     user_store.append(user_label.copy())
 print(user_store)
-
+def refresh_list(get_list):
+    global today_list_time
+    if time.time() - today_list_time > 1800:    #每三十分钟更新一次招标信息
+        get_list = sunshine_list()
+        today_list_time = time.time()
+    return get_list
 
 def refresh_token():
     global access_token
@@ -36,6 +42,7 @@ def refresh_token():
 @robot.handler
 def echo(msg):
     refresh_token()
+    refresh_list(today_list)
     print(msg.source)
     if msg.content == '1':
         for j in user_store:  # 遍历user_store
