196a53c2bc4cf421bce1ac9683725ade75167448
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-09 02:27:19 +0800

    robot 1.1 每三十分钟更新一次招标信息

--- robot.py
+++ robot.py
@@ -46,36 +46,36 @@ def echo(msg):
     refresh_token()
     refresh_list(today_list)
     print(msg.source)
-    if msg.content == '2':
+    if msg.content == '1':
         for j in user_store:  # 遍历user_store
             if j['openid'] == msg.source:  # 检索库中的用户id
-                if len(hot_film) - j['film_send'] > 10:  # 如果存在未发数据
-                    wait_to_send = hot_film[j['film_send']:j['film_send'] + 10]  # 则切片最多最多3条并发送
+                if len(today_list) - j['last_send'] > 10:  # 如果存在未发数据
+                    wait_to_send = today_list[j['last_send']:j['last_send'] + 10]  # 则切片最多最多3条并发送
                     for k in range(len(wait_to_send)):
-                        send_msg(wait_to_send[k], msg.source, access_token)
-                    j['film_send'] += 10  # 发送完成，更新用户的发送标记
-                    return "还有%s条"%(len(hot_film) - j['film_send'])
+                        send_msg(wait_to_send[k]['docTitle'], msg.source, access_token)
+                    j['last_send'] += 10  # 发送完成，更新用户的发送标记
+                    return "还有%s条"%(len(today_list) - j['last_send'])
                 else:
-                    wait_to_send = hot_film[j['film_send']::]
+                    wait_to_send = today_list[j['last_send']::]
                     for k in range(len(wait_to_send)):
-                        send_msg(wait_to_send[k], msg.source, access_token)
-                    j['film_send'] = 0  # 发送完成，清除标记
+                        send_msg(wait_to_send[k]['docTitle'], msg.source, access_token)
+                    j['last_send'] = 0  # 发送完成，清除标记
                     return '发送完毕'
     else:
-        if msg.content == '1':
+        if msg.content == '2':
             for j in user_store:  # 遍历user_store
                 if j['openid'] == msg.source:  # 检索库中的用户id
-                    if len(today_list) - j['last_send'] > 10:  # 如果存在未发数据
-                        wait_to_send = today_list[j['last_send']:j['last_send'] + 10]  # 则切片最多最多3条并发送
+                    if len(hot_film) - j['film_send'] > 10:  # 如果存在未发数据
+                        wait_to_send = hot_film[j['film_send']:j['film_send'] + 10]  # 则切片最多最多3条并发送
                         for k in range(len(wait_to_send)):
-                            send_msg(wait_to_send[k]['docTitle'], msg.source, access_token)
-                        j['last_send'] += 10  # 发送完成，更新用户的发送标记
-                        return "还有%s条" % (len(today_list) - j['last_send'])
+                            send_msg(wait_to_send[k], msg.source, access_token)
+                        j['film_send'] += 10  # 发送完成，更新用户的发送标记
+                        return "还有%s条" % (len(hot_film) - j['film_send'])
                     else:
-                        wait_to_send = today_list[j['last_send']::]
+                        wait_to_send = hot_film[j['film_send']::]
                         for k in range(len(wait_to_send)):
-                            send_msg(wait_to_send[k]['docTitle'], msg.source, access_token)
-                        j['last_send'] = 0  # 发送完成，清除标记
+                            send_msg(wait_to_send[k], msg.source, access_token)
+                        j['film_send'] = 0  # 发送完成，清除标记
                         return '发送完毕'
 
         return '1---电信招标网（阳光）\n 2---热门影视'
