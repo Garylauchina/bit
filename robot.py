cea2ebd98615d5097ee128e917c76ac6566e09df
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-09 18:30:41 +0800

    发送十条就停了，还没找到原因

--- robot.py
+++ robot.py
@@ -3,10 +3,9 @@ import werobot
 from sunshine import *
 import time
 
-
 now = time.strftime('%Y-%m-%d %H:%M:%S')
 print('启动时间：' + now)
-print("版本1.1 加入supervisor进程")
+print("版本1.2 通过git更新后，supervisor中重启服务")
 robot = werobot.WeRoBot(token='Dcbpes2098')
 access_token = Access_Token()
 token_time = int(time.time())
@@ -25,11 +24,11 @@ for i in user_list:
     user_label['last_send'] = 0
     user_label['film_send'] = 0
     user_store.append(user_label.copy())
-print('总共%s名用户'%len(user_store))
+print('总共%s名用户' % len(user_store))
 
 
 def refresh_list(get_id):
-    global today_list,hot_film,today_list_time,hot_film_time
+    global today_list, hot_film, today_list_time, hot_film_time
     if get_id == 1:
         if time.time() - today_list_time > 1800:  # 每三十分钟更新一次招标信息
             today_list = sunshine_list()
@@ -43,11 +42,9 @@ def refresh_list(get_id):
                 print('电影列表已更新')
     return
 
-    return get_time
-
 
 def refresh_token():
-    global access_token,token_time
+    global access_token, token_time
     if int(time.time()) - token_time >= 3600:
         access_token = Access_Token()
         token_time = int(time.time())
@@ -56,12 +53,13 @@ def refresh_token():
         print('token未过期，有效时间%s秒' % (7200 - (int(time.time()) - token_time)))
     return
 
-def send_all(get_list,get_id):
-    global user_store,access_token
+
+def send_all(get_list, get_id):
+    global user_store, access_token
     for j in user_store:  # 遍历user_store
         if j['openid'] == get_id:
             if len(get_list) - j['last_send'] > 10:  # 如果存在未发数据
-                wait_to_send = get_list[j['last_send']:j['last_send'] + 10]  # 则切片最多最多3条并发送
+                wait_to_send = get_list[j['last_send']:j['last_send'] + 10]  # 则切片最多最多10条并发送
                 for k in wait_to_send:
                     send_msg(k, get_id, access_token)
                 j['last_send'] += 10  # 发送完成，更新用户的发送标记
@@ -73,19 +71,20 @@ def send_all(get_list,get_id):
                 j['last_send'] = 0  # 发送完成，清除标记
     return
 
+
 @robot.handler
 def echo(msg):
-    global today_list_time,hot_film
+    global today_list_time, hot_film
     refresh_token()
     print(msg.source)
     if msg.content == '1':
         refresh_list(1)
-        send_all(today_list,msg.source)
+        send_all(today_list, msg.source)
         return '发送完毕'
     else:
         if msg.content == '2':
             refresh_list(2)
-            send_all(hot_film,msg.source)
+            send_all(hot_film, msg.source)
             return '发送完毕'
     return '1---电信招标网（阳光）\n 2---热门影视'
 
