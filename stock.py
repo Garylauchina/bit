f4ea1c4701ca38fc7f2e05614a63b6a27d589228
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-10 15:22:20 +0800

    加入股票价格查询

--- stock.py
+++ stock.py
@@ -10,8 +10,10 @@ def get_stock(stock_name):
         r = re.findall(r"_ntes_quote_callback\((.*)\);", r)
         r = r[0]
         r = json.loads(r)
-        result = r[stock_name]['name'] + '\n' + str(r[stock_name]['price'])
+        result = '名称：' + r[stock_name]['name'] + '\n' + '当前价格：' + str(
+            r[stock_name]['price']) + '\n' + '涨跌：' + str(
+            r[stock_name]['percent']) + '\n' + '开盘价格：' + str(r[stock_name]['open']) + '\n' + '成交额：' + str(
+            r[stock_name]['turnover'])
     except:
         result = 'SH:0xxxxxx\nSZ:1xxxxxx\nBJ:2xxxxxx'
     return result
-
