ea1d9fa07ae5f649310aaae1b9dfe813b224fd2e
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-13 00:58:18 +0800

    解决了移动网二次传参的问题

--- chinamobile.py
+++ chinamobile.py
@@ -2,42 +2,6 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-'''
-def cm_new_list():
-    requests.packages.urllib3.disable_warnings()
-    url = 'https://b2b.10086.cn/b2b/main/listVendorNoticeResult.html?'
-    data = {
-        'page.currentPage': 1,
-        'page.perPageSize': 20,
-        'noticeBean.sourceCH': '广西',
-        'noticeBean.source': 'GX',
-        'noticeBean.title': '',
-        'noticeBean.startDate': '',
-        'noticeBean.endDate': '',
-        '_qt': 'WO5ITMMzN0UDZmdzN1EGOyIDO5QWN2YTNwgzY2UzY4E',
-    }
-    headers = {
-        'Cookie': 'JSESSIONID=682AC9A5D2280FD69D9E5F28F84BFB7D',
-        'Referer': 'https://b2b.10086.cn/b2b/main/listVendorNotice.html?noticeType=2',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/105.0.0.0 Safari/537.36',
-    }
-
-    r = requests.post(url, data=data, headers=headers, verify=False)
-    soup = BeautifulSoup(r.text, 'lxml')
-    td = soup.find_all('td')
-    all_list = []
-    for i in td:
-        # print(i)
-        try:
-            k = i.a.text
-            all_list.append(k)
-        except:
-            pass
-    del all_list[-4:]
-    return all_list
-'''
-
 
 # 解决了二次传送post参数的问题
 def cm_new_list():
