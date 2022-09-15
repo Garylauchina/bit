a39bfa86b3089353432ea022cb9190d6c8e32a7a
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-15 17:19:01 +0800

    网易的股票接口有问题，改用tushare接口

---
+++ 重写一遍，加入数据库和类方法/robot.py
@@ -0,0 +1,15 @@
+"""
+目标1：熟练掌握enumerate命令进行列表操作
+目标2：初步掌握用类进行编程
+目标3：初步掌握函数装饰器的用法
+目标4：初步掌握SQLite的用法
+目标5：掌握后台爬虫定时工作的方法
+"""
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Python')
---
+++ 重写一遍，加入数据库和类方法/stock.py
@@ -0,0 +1,10 @@
+
+class Stock(object):
+    def __init__(self,name):
+        self.assertEqual(True, False)  # add assertion here
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
