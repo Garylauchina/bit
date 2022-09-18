bc8789916219c3b050236a922c19632e375d5f6f
Garylauchina <103692966+Garylauchina@users.noreply.github.com>
2022-09-19 02:32:36 +0800

    1.5 加入远程更新和重启功能

--- joke.py
+++ joke.py
@@ -6,12 +6,12 @@ def get_joke():
     r = requests.get(url)
     if r.status_code != 200:
         return '今天没笑话'
-    return r.json()['content']
+    r = r.json()['content']
+    r = r.replace('提示：按分类看笑话请发送“笑话分类”', '')
+    r = r.replace('{br}', '\n')
+    return r
 
 
 if __name__ == '__main__':
     joke = get_joke()
-    print(type(joke))
-    joke = joke.replace('提示：按分类看笑话请发送“笑话分类”', '')
-    joke = joke.replace('{br}', '\n')
     print(joke)
